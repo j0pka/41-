11d0edc101708b8a87b49be9a2d751b1808897d2
j0pka <jo.pka@icloud.com>
2024-01-29 21:55:35 +0300

    16 laba + скрипт БД

---
+++ 16/Bocharova41/Bocharova41/HeadPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bocharova41
+{
+    /// <summary>
+    /// Логика взаимодействия для HeadPage.xaml
+    /// </summary>
+    public partial class HeadPage : Page
+    {
+        public HeadPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }//удалить
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+    }
+}
