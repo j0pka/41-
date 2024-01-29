e963629153968d626f694598fb78b64687f466e8
j0pka <jo.pka@icloud.com>
2024-01-29 21:56:48 +0300

    17 laba

---
+++ 17/Bocharova41/Bocharova41/HeadPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bocharova41
+{
+    /// <summary>
+    /// Логика взаимодействия для HeadPage.xaml
+    /// </summary>
+    public partial class HeadPage : Page
+    {
+        public HeadPage()
+        {
+            InitializeComponent();
+            var currentRazmer = Bocharova41Entities.GetContext().Product.ToList();
+            RazmerListView.ItemsSource = currentRazmer;
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }//удалить
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
