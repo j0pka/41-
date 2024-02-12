2760ee60d784e10fcd0fd749ce10e4da0ed75077
j0pka <jo.pka@icloud.com>
2024-02-12 17:47:45 +0300

    19 laba

---
+++ 19/Bocharova41/Bocharova41/AuthPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Bocharova41
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        
+        DispatcherTimer timer = new DispatcherTimer();
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GuestLoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new HeadPage(null));
+            LoginTB.Text = "";
+            PassTB.Text = "";
+            timer.Stop();
+            LoginBtn.IsEnabled = true;
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTB.Text;
+            string password = PassTB.Text;
+            if (login == "" || password == "")
+            {
+                MessageBox.Show("Введите данные");
+                return;
+            }
+
+            User user = Bocharova41Entities.GetContext().User.ToList().Find(p => p.UserLogin == login && p.UserPassword == password);
+
+            if (user != null)
+            {
+                Manager.MainFrame.Navigate(new HeadPage(user));
+                LoginTB.Text = "";
+                PassTB.Text = "";
+            }
+            
+            else
+            {
+                
+                MessageBox.Show("Введите верные данные!");
+                LoginBtn.IsEnabled = false;
+                
+                timer.Interval = TimeSpan.FromSeconds(2);
+                timer.Start();
+
+                timer.Tick += (senderr, er) =>
+                {
+                    LoginBtn.IsEnabled = true;
+                    timer.Stop();
+                };
+
+                
+            }
+            
+        }
+    }
+}
---
+++ 19/Bocharova41/Bocharova41/HeadPage.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bocharova41
+{
+    /// <summary>
+    /// Логика взаимодействия для HeadPage.xaml
+    /// </summary>
+    public partial class HeadPage : Page
+    {
+        int CountRecords;
+        int AllProductsAmount;
+        public HeadPage(User user)
+        {
+            InitializeComponent();
+            var currentRazmer = Bocharova41Entities.GetContext().Product.ToList();
+            RazmerListView.ItemsSource = currentRazmer;
+            ComboType.SelectedIndex = 0;
+            UpdateProduct();
+            if (user != null)
+            {
+                FIOTB.Text = user.UserSurname + " " + user.UserName + " " + user.UserPatronymic;
+                switch (user.UserRole)
+                {
+                    case 1:
+                        RoleTB.Text = "Клиент"; break;
+                    case 2:
+                        RoleTB.Text = "Менеджер"; break;
+                    case 3:
+                        RoleTB.Text = "Администратор"; break;
+                    default: 
+                        RoleTB.Text = "Гость"; break;
+                }
+            }
+            else
+            {
+                FIOTB.Text = "Гость";
+                RoleTB.Text = "Гость";
+            }
+
+            UpdateProduct();
+
+        }
+         private void UpdateProduct()
+        {
+            var currentRazmer = Bocharova41Entities.GetContext().Product.ToList();
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentRazmer = currentRazmer.Where(p => Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) <= 100).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentRazmer = currentRazmer.Where(p => Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) < 10).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentRazmer = currentRazmer.Where(p => Convert.ToInt32(p.ProductDiscountAmount) >= 10 && Convert.ToInt32(p.ProductDiscountAmount) < 15).ToList();
+            }
+
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentRazmer = currentRazmer.Where(p => Convert.ToInt32(p.ProductDiscountAmount) >= 15 && Convert.ToInt32(p.ProductDiscountAmount) <= 100).ToList();
+            }
+
+            currentRazmer = currentRazmer.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            RazmerListView.ItemsSource = currentRazmer;
+            CountRecords = currentRazmer.Count();
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                RazmerListView.ItemsSource = currentRazmer.OrderByDescending(p => p.ProductCost);
+                CountRecords = currentRazmer.OrderByDescending(p => p.ProductCost).Count();
+            }
+
+            if (RButtinUp.IsChecked.Value)
+            {
+                RazmerListView.ItemsSource = currentRazmer.OrderBy(p => p.ProductCost);
+                CountRecords = currentRazmer.OrderBy(p => p.ProductCost).Count();
+            }
+
+            AllProductsAmount = Bocharova41Entities.GetContext().Product.ToList().Count();
+
+            ProductsAmount.Text = AllProductsAmount.ToString();
+            ProductsCount.Text = CountRecords.ToString();
+        }
+
+        
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }//удалить
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void RButtinUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+    }
+}
