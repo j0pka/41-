18c189c31644acae92ca4811ece3af839e8aa054
j0pka <jo.pka@icloud.com>
2024-02-12 16:34:56 +0300

    18 laba

---
+++ 18/Bocharova41/Bocharova41/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bocharova41
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new HeadPage());
+            Manager.MainFrame = MainFrame;
+        }
+
+        private void BtnBaack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BtnBaack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnBaack.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
